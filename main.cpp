77d52e87ebeb7e0b592a6c94318c787e86f903f9
AleTodaroCbr650r <115482662+AleTodaroCbr650r@users.noreply.github.com>
2022-11-27 22:46:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int a=1, b=1, c=0, n=0;
+    cin >> n;
+    for(int i=0;i>-1;i++){
+        if(a<=n){
+            cout << a << endl;
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+  
+ 
    return 0;
 }
